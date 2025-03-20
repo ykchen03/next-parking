a36cf62d4792cdafe5652353a8590aa151b2b821
ykchen03 <ykchen03@gmail.com>
2025-03-20 11:45:02 +0800

    Add files via upload

--- app/api/neon/route.js
+++ app/api/neon/route.js
@@ -5,6 +5,7 @@ const sql = neon(process.env.DATABASE_URL);
 export async function GET(req) {
   //const { lon, lat, radius } = req.query;
   const searchParams = req.nextUrl.searchParams;
+  const city = searchParams.get("city");
   const lon = searchParams.get("lon");
   const lat = searchParams.get("lat");
   const radius = searchParams.get("radius");
@@ -12,21 +13,25 @@ export async function GET(req) {
   const longitude = parseFloat(lon);
   const latitude = parseFloat(lat);
   const searchRadius = parseFloat(radius);
-  
-  if (isNaN(longitude) || isNaN(latitude) || isNaN(searchRadius)) {
-    return res.status(400).json({ 
-      error: "Invalid parameters. lon, lat, and radius must be valid numbers" 
-    });
-  }
-
-  try {
-    const data = await sql`
+  console.log(`
     SELECT name, distance
     FROM ( SELECT name,
             ST_Distance(location,ST_SetSRID(ST_MakePoint(${lon},${lat}), 4326)::geography)
-            AS distance FROM parking_lots) AS subquery
+            AS distance FROM ${city}) AS subquery
     WHERE distance <= ${radius};
-    `;
+    `);
+  
+  if (!city || isNaN(longitude) || isNaN(latitude) || isNaN(searchRadius)) {
+    return Response.json({ error: "Invalid query parameters" }, { status: 400 });
+  }
+
+  try {
+    const data = await sql(`SELECT name, distance
+    FROM ( SELECT name,
+            ST_Distance(location,ST_SetSRID(ST_MakePoint(${lon},${lat}), 4326)::geography)
+            AS distance FROM ${city}) AS subquery
+    WHERE distance <= ${radius};`);
+    
     return Response.json(data, { status: 200 });
     //return res.status(200).json(data);
   } catch (error) {
