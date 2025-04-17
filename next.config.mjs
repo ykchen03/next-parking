2969f4032dce6148268a48e2b8d9dcbbe23f3912
ykchen03 <ykchen03@gmail.com>
2025-04-17 12:14:18 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    rewrites: async () => {
-      return [
-        {
-          source: "/api/py/:path*",
-          destination:
-            process.env.NODE_ENV === "development"
-              ? "http://127.0.0.1:8000/api/py/:path*"
-              : "/api/",
-        },
-      ];
-    },
-  };
+const nextConfig = {};
 
 export default nextConfig;
