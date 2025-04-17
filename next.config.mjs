a0801a4731339a4a272bb6d27f01697f12490279
ykchen03 <ykchen03@gmail.com>
2025-04-17 10:07:29 +0800

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    rewrites: async () => {
+      return [
+        {
+          source: "/api/py/:path*",
+          destination:
+            process.env.NODE_ENV === "development"
+              ? "http://127.0.0.1:8000/api/py/:path*"
+              : "/api/",
+        },
+      ];
+    },
+  };
 
 export default nextConfig;
